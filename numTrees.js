c90300feb58e8a88ad5748b6bd0696c1358c691f
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-04-11 20:44:27 -0700

    added solution to unique BST

--- numTrees.js
+++ numTrees.js
@@ -9,15 +9,23 @@ const numTrees = n => {
     dp[0] = 0
     dp[1] = 1
 
+    for (let i = 2; i <= n; i++) {
+        let left = 0, right = i-1, product = 0
 
+        for (let j = left; j < i; left++, right--, j++) {
+            if (left === 0) {
+                product+=dp[right]
+            } else if (right === 0) {
+                product+=dp[left]
+            } else {
+                product+=(dp[left]*dp[right])
+            }
+        }
 
-    for (let i = 2; i < n; i++) {
+        dp[i] = product
         
-
-        dp[i] +=
     }
 
-    console.log(dp)
     return dp[n]
 };
 
