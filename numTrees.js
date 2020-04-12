44ba0a26107a31d6196300a14d20d1a8018f6e73
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-04-11 21:30:02 -0700

    added a more sound optimized solution

--- numTrees.js
+++ numTrees.js
@@ -6,24 +6,14 @@
 
 const numTrees = n => {
     const dp = new Array(n+1).fill(0)
-    dp[0] = 0
+    dp[0] = 1
     dp[1] = 1
 
     for (let i = 2; i <= n; i++) {
-        let left = 0, right = i-1, product = 0
-
+        let left = 0, right = i-1
         for (let j = left; j < i; left++, right--, j++) {
-            if (left === 0) {
-                product+=dp[right]
-            } else if (right === 0) {
-                product+=dp[left]
-            } else {
-                product+=(dp[left]*dp[right])
-            }
+            dp[i]+=(dp[left]*dp[right])
         }
-
-        dp[i] = product
-        
     }
 
     return dp[n]
