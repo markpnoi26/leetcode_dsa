1774b0579810ed3d2d97f0ee586f731436f27041
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-04-11 21:32:21 -0700

    added notes to the solution: Catalan numbers using DP

--- numTrees.js
+++ numTrees.js
@@ -8,6 +8,12 @@ const numTrees = n => {
     const dp = new Array(n+1).fill(0)
     dp[0] = 1
     dp[1] = 1
+    /**
+     * dp[0] was originally thought of as => 0 but setting it to 1
+     * made it possible to multiply the right side to the left side of the unique combinations
+     * of the BST. The logic made it easier and faster than the original thought of solution.
+     */
+
 
     for (let i = 2; i <= n; i++) {
         let left = 0, right = i-1
