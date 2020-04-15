b83dd32d944249449765f66d1f83d495975927e3
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-04-15 13:31:04 -0700

    added solution via recursion

---
+++ buildTree.js
@@ -0,0 +1,75 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+
+class TreeNode{
+    constructor(val) {
+        this.val = val
+        this.left = null
+        this.right = null
+    }
+}
+
+const buildTree = (preorder, inorder) => {
+    
+    if (preorder.length === 0 && inorder.length === 0) return null
+    
+    if (preorder.length === 1 && inorder.length === 1) {
+        let baseVal = new TreeNode(preorder[0])
+        baseVal.right = null
+        baseVal.left = null
+        return baseVal
+    }
+
+    let subRoot = new TreeNode(preorder[0])
+    let leftPre, leftIn, rightPre, rightIn, mid
+
+    for (let i=0;i<inorder.length; i++) {
+        if (inorder[i] === preorder[0]) {
+            leftIn = inorder.slice(0,i)
+            rightIn = inorder.slice(i+1)
+            mid = i
+        }
+    }
+
+
+    leftPre = preorder.slice(1, mid+1)
+    rightPre = preorder.slice(mid+1)
+
+    console.log(rightIn, leftIn)
+    console.log(leftPre, rightPre)
+
+    
+
+    subRoot.left = buildTree(leftPre, leftIn)
+    subRoot.right = buildTree(rightPre, rightIn)
+
+
+
+    return subRoot
+    
+};
+
+
+let preorder, inorder;
+
+preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+console.log(buildTree(preorder,inorder))
+
+preorder = [3,9,15,20,7], inorder = [9,15,3,20,7]
+console.log(buildTree(preorder,inorder))
+
+preorder = [9], inorder = [9]
+console.log(buildTree(preorder,inorder))
+
+preorder = [9,15], inorder = [9,15]
+console.log(buildTree(preorder,inorder))
